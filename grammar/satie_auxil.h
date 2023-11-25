8502a37ec3c146fd3825030e06a307c68d62c8c3
Joakim G <jocke@gmail.com>
2023-11-25 18:30:39 +0100

    being anal

--- grammar/satie_auxil.h
+++ grammar/satie_auxil.h
@@ -10,7 +10,6 @@ typedef struct {
 static void panic(const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
-    fprintf(stderr, "Syntax error: ");
     vfprintf(stderr, fmt, args);
     fprintf(stderr, "\n");
     va_end(args);
