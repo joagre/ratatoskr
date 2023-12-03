835a29819b933c3f8577604c3beb8f69db3e7bc0
Joakim G <jocke@gmail.com>
2023-12-03 13:03:15 +0100

    being anal

---
+++ vm/cc/call_stack.c
@@ -0,0 +1,139 @@
+#define MUTE_LOG_DEBUG 1
+
+#include <stdio.h>
+#include "call_stack.h"
+#include "log.h"
+
+void call_stack_init(call_stack_t* call_stack, stack_t* initial_stack) {
+                     //                     data_stack_t* data_stack) {
+    call_stack->stack = initial_stack;
+    call_stack->fp = 0;
+    //call_stack->data_stack = data_stack;
+}
+
+void call_stack_free(call_stack_t* call_stack) {
+    dynarray_clear(call_stack->stack);
+}
+
+size_t call_stack_length(call_stack_t* call_stack) {
+    return dynarray_size(call_stack->stack);
+}
+
+void call_stack_push(call_stack_t* call_stack, vm_stack_value_t value) {
+    dynarray_append(call_stack->stack, &value);
+}
+
+vm_stack_value_t call_stack_pop_string(call_stack_t*) {
+    /*
+    vm_stack_value_t* data_address = dynarray_pop(call_stack->stack);
+    uint8_t* bytes = data_stack_peek(call_stack->data_stack, *data_address);
+    vm_stack_value_t value =
+        GET_VALUE(vm_stack_value_t, bytes + sizeof(vm_data_length_t));
+    return value;
+    */
+    return 0;
+}
+
+vm_stack_value_t call_stack_pop(call_stack_t* call_stack) {
+    vm_stack_value_t* value = dynarray_pop(call_stack->stack);
+    return *value;
+}
+
+void call_stack_dup(call_stack_t* call_stack) {
+    vm_stack_value_t* value =
+        dynarray_element(call_stack->stack,
+                         dynarray_size(call_stack->stack) - 1);
+    dynarray_append(call_stack->stack, value);
+}
+
+void call_stack_swap(call_stack_t* call_stack) {
+    if (dynarray_size(call_stack->stack) < 2) {
+        LOG_ERROR("Stack underflow");
+        return;
+    }
+    vm_stack_value_t* top = dynarray_pop(call_stack->stack);
+    vm_stack_value_t* second_top = dynarray_pop(call_stack->stack);
+    vm_stack_value_t temp = *second_top;
+    dynarray_append(call_stack->stack, top);
+    dynarray_append(call_stack->stack, &temp);
+}
+
+void call_stack_load(call_stack_t* call_stack) {
+    vm_stack_value_t offset = call_stack_pop(call_stack);
+    vm_stack_value_t* value =
+        dynarray_element(call_stack->stack, call_stack->fp + offset);
+    dynarray_append(call_stack->stack, value);
+}
+
+void call_stack_store(call_stack_t* call_stack) {
+    vm_stack_value_t* value = dynarray_pop(call_stack->stack);
+    vm_stack_value_t offset = call_stack_pop(call_stack);
+    dynarray_setelement(call_stack->stack, call_stack->fp + offset, value);
+}
+
+void call_stack_binary_operation(call_stack_t* call_stack,
+                                 vm_stack_value_t (*fun)(vm_stack_value_t,
+                                                         vm_stack_value_t)) {
+    vm_stack_value_t operand2 = call_stack_pop(call_stack);
+    vm_stack_value_t operand1 = call_stack_pop(call_stack);
+    call_stack_push(call_stack, fun(operand1, operand2));
+}
+
+void call_stack_print(call_stack_t* call_stack) {
+    size_t i;
+    for (i = 0; i < dynarray_size(call_stack->stack); i++) {
+        vm_stack_value_t* value = dynarray_element(call_stack->stack, i);
+        LOG_DEBUG("stack[%d] = %ld", i, *value);
+    }
+}
+
+//
+// Unit test
+//
+
+static vm_stack_value_t add(vm_stack_value_t a, vm_stack_value_t b) {
+    return a + b;
+}
+
+void call_stack_unit_test(void) {
+    call_stack_t call_stack;
+
+    // Initial empty stack
+    stack_t stack;
+    dynarray_init(&stack, NULL, 512, sizeof(vm_stack_value_t));
+
+    // Push 1
+    call_stack_init(&call_stack, &stack);
+    call_stack_push(&call_stack, 1);
+    LOG_ASSERT(call_stack_pop(&call_stack) == 1, "call_stack_pop");
+    LOG_ASSERT(call_stack_length(&call_stack) == 0, "call_stack_length");
+
+    // Push 1, 2
+    call_stack_push(&call_stack, 1);
+    call_stack_push(&call_stack, 2);
+    call_stack_print(&call_stack);
+
+    // Swap
+    call_stack_print(&call_stack);
+    call_stack_swap(&call_stack);
+    call_stack_print(&call_stack);
+    LOG_ASSERT(call_stack_pop(&call_stack) == 1, "call_stack_pop");
+    LOG_ASSERT(call_stack_pop(&call_stack) == 2, "call_stack_pop");
+
+    // Dup
+    call_stack_push(&call_stack, 1);
+    call_stack_push(&call_stack, 2);
+    call_stack_dup(&call_stack);
+
+    // Binary operation
+    LOG_ASSERT(call_stack_pop(&call_stack) == 2, "call_stack_pop");
+    LOG_ASSERT(call_stack_pop(&call_stack) == 2, "call_stack_pop");
+    call_stack_push(&call_stack, 1);
+    call_stack_push(&call_stack, 2);
+    call_stack_binary_operation(&call_stack, add);
+    LOG_ASSERT(call_stack_pop(&call_stack) == 3, "call_stack_pop");
+    LOG_ASSERT(call_stack_length(&call_stack) == 1, "call_stack_length");
+    call_stack_free(&call_stack);
+
+    LOG_INFO("call_stack_unit_test passed");
+}
---
+++ vm/cc/call_stack.h
@@ -0,0 +1,32 @@
+#ifndef __CALL_STACK_H__
+#define __CALL_STACK_H__
+
+#include "clib/dynarr.h"
+#include "vm.h"
+
+typedef dynarray_t stack_t;
+
+typedef struct {
+    stack_t* stack;
+    vm_stack_value_t fp;
+    //data_stack_t* data_stack;
+} call_stack_t;
+
+void call_stack_init(call_stack_t* call_stack, stack_t* initial_stack);
+                     //                     data_stack_t* data_stack);
+void call_stack_free(call_stack_t* call_stack);
+size_t call_stack_length(call_stack_t* call_stack);
+void call_stack_push(call_stack_t* call_stack, vm_stack_value_t value);
+vm_stack_value_t call_stack_pop_string(call_stack_t* call_stack);
+vm_stack_value_t call_stack_pop(call_stack_t* call_stack);
+void call_stack_dup(call_stack_t* call_stack);
+void call_stack_swap(call_stack_t* call_stack);
+void call_stack_load(call_stack_t* call_stack);
+void call_stack_store(call_stack_t* call_stack);
+void call_stack_binary_operation(call_stack_t* call_stack,
+                                 vm_stack_value_t (*fun)(vm_stack_value_t,
+                                                         vm_stack_value_t));
+void call_stack_print(call_stack_t* call_stack);
+void call_stack_unit_test(void);
+
+#endif
