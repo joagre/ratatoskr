2154aa30965a4741b6a1563c85c7597cd06b310b
Joakim G <jocke@gmail.com>
2023-12-04 01:48:47 +0100

    being anal

---
+++ vm/cc/data_stack.c
@@ -0,0 +1,29 @@
+#include "data_stack.h"
+
+void data_stack_init(data_stack_t* data_stack) {
+    dynarray_init(&data_stack->stack_array, NULL, 512, sizeof(uint8_t));
+    data_stack->fp = 0;
+}
+
+void data_stack_free(data_stack_t* data_stack) {
+    dynarray_clear(&data_stack->stack_array);
+}
+
+size_t data_stack_length(data_stack_t* data_stack) {
+    return dynarray_size(&data_stack->stack_array);
+}
+
+vm_data_length_t data_stack_push(data_stack_t* data_stack, uint8_t* bytes,
+                                 vm_address_t* data_address) {
+    vm_data_length_t length = GET_VALUE(vm_data_length_t, bytes);
+    *data_address = dynarray_size(&data_stack->stack_array);
+    for (size_t i = 0; i < length; i++) {
+        dynarray_append(&data_stack->stack_array,
+                        bytes + sizeof(vm_data_length_t) + i);
+    }
+    return length;
+}
+
+uint8_t* data_stack_peek(data_stack_t* data_stack, vm_address_t data_address) {
+    return dynarray_element(&data_stack->stack_array, data_address);
+}
---
+++ vm/cc/data_stack.h
@@ -0,0 +1,21 @@
+#ifndef __DATA_STACK_H__
+#define __DATA_STACK_H__
+
+#include "clib/dynarr.h"
+#include "vm.h"
+
+typedef dynarray_t data_stack_array_t;
+
+typedef struct {
+    data_stack_array_t stack_array;
+    vm_stack_value_t fp;
+} data_stack_t;
+
+void data_stack_init(data_stack_t* data_stack);
+void data_stack_free(data_stack_t* data_stack);
+size_t data_stack_length(data_stack_t* data_stack);
+vm_data_length_t data_stack_push(data_stack_t* data_stack, uint8_t* bytes,
+                                 vm_address_t* data_address);
+uint8_t* data_stack_peek(data_stack_t* data_stack, vm_address_t data_address);
+
+#endif
